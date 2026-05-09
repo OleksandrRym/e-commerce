d05a9f3af5c5649e1605969d7869260391b61914
OleksandrRym <lostmannight@gmail.com>
2026-05-09 21:44:45 +0300

    init

---
+++ demo/core-service/src/test/java/com/rymar/coreservice/CoreServiceApplicationTests.java
@@ -0,0 +1,11 @@
+package com.rymar.coreservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoreServiceApplicationTests {
+
+  @Test
+  void contextLoads() {}
+}
